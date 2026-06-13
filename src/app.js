5f23cb31cced73a1aad3a3baf897c6bb7c494841
Abdullah <abdallahbeedo855@gmail.com>
2026-06-13 21:28:16 +0300

    specifying the allowed methods

--- src/app.js
+++ src/app.js
@@ -17,8 +17,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL ,
-     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    origin: [
+    "http://localhost:5173",
+    process.env.CLIENT_URL, 
+  ] ,
+     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
