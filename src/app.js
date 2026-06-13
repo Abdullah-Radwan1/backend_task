58c185ebb98f32eb269bf385974be6d81de4edd9
Abdullah <abdallahbeedo855@gmail.com>
2026-06-13 21:11:35 +0300

    removing local-url from cors

--- src/app.js
+++ src/app.js
@@ -14,23 +14,10 @@ dotenv.config();
 const app = express();
 
 // Global Middlewares
-const allowedOrigins = [
-  "http://localhost:5174",
-  "https://task-front-eta.vercel.app",
-];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow mobile apps / postman (no origin)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: process.env.CLIENT_URL ,
     credentials: true,
   })
 );
